30c06fe9831536f548feaec61a69b1dd149517a9
SRIKANTH B <112770979+Sri2413@users.noreply.github.com>
2026-01-06 00:57:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Cart from "./Pages/Cart";
 import Home from "./Pages/Home";
 import Success from "./Pages/Success";
+import Failure from "./Pages/Failure";
 function App() {
   return (
     <BrowserRouter>
@@ -9,6 +10,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/payment-success" element={<Success />} />
+        <Route path="/payment-failed" element={<Failure />} />
       </Routes>
     </BrowserRouter>
   );
